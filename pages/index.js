8c58b08547ce9446c7dcfe65ca4e6056f94b7c8d
Amorem <dev@amorem.com>
2020-03-25 07:56:14 +0100

    export

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
+import fs from "fs";
 
-const Home = () => (
+const Home = ({ slugs }) => (
   <div>
-    <Link href="/faq">
-      <a>go to faq</a>
-    </Link>
+    {slugs.map(slug => (
+      <div key={slug}>
+        <Link href={`/blog/${slug}`}>
+          <a>{slug}</a>
+        </Link>
+      </div>
+    ))}
   </div>
 );
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync("posts");
+  return {
+    props: {
+      slugs: files.map(filename => filename.replace(".md", ""))
+    }
+  };
+};
 
 export default Home;
